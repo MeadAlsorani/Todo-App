03d5cba899054a3737836a1752c5b082b338a064
Mead Alsorani <maadsorani24@gmail.com>
2023-07-30 17:15:08 +0300

    Update Item with Colour Test

---
+++ src/Application/TodoItems/Queries/GetTodoItem/GetTodoItemQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Todo_App.Application.Common.Interfaces;
+using Todo_App.Application.TodoLists.Queries.GetTodos;
+
+namespace Todo_App.Application.TodoItems.Queries.GetTodoItem;
+
+public record GetTodoItemQuery : IRequest<TodoItemDto>
+{
+    public int Id { get; set; }
+}
+
+public class GetTodoItemQueryHandler : IRequestHandler<GetTodoItemQuery, TodoItemDto>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetTodoItemQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<TodoItemDto> Handle(GetTodoItemQuery request, CancellationToken cancellationToken)
+    {
+
+        return await _context.TodoItems
+            .ProjectTo<TodoItemDto>(_mapper.ConfigurationProvider)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(i => i.Id == request.Id);
+    }
+}
